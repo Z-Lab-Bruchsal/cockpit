55e813b4282aeab3c3776fc08077b7e4b9a28cef
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-02-17 00:34:02 +0000

    ordermanager muss fillable sein

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements FilamentUser
         'name',
         'email',
         'password',
+        'ordermanager',
     ];
 
     public function canAccessPanel(Panel $panel): bool
