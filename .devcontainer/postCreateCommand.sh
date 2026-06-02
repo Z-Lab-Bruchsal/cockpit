f82d3f59e0d246a84075c96c8dd7703e4d323ba4
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-06-02 15:45:52 +0000

    updated devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,6 +11,6 @@ ${SUDO} dpkg -i /tmp/debsuryorg-archive-keyring.deb
 ${SUDO} sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 ${SUDO} apt-get update
 
-${SUDO} apt-get -y install plantuml php8.5 php8.5-{cli,zip,sqlite3,xml,pgsql,bcmath,intl,mbstring} composer npm sqlite3
+${SUDO} apt-get -y install plantuml php8.5 php8.5-{cli,zip,sqlite3,xml,pgsql,bcmath,intl,mbstring,curl} composer sqlite3
 composer install
 npm install
