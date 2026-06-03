8d887de837d8f429c0068f8bb45370a044f8cf7a
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-06-03 20:03:24 +0000

    cleanup infolist

--- app/Filament/Resources/Courses/RelationManagers/KidsRelationManager.php
+++ app/Filament/Resources/Courses/RelationManagers/KidsRelationManager.php
@@ -13,7 +13,6 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
-use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
@@ -37,23 +36,6 @@ class KidsRelationManager extends RelationManager
                 TextInput::make('email')
                     ->label('Email address')
                     ->email(),
-            ]);
-    }
-
-    public function infolist(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('name'),
-                TextEntry::make('email')
-                    ->label('Email address')
-                    ->placeholder('-'),
             ]);
     }
 
