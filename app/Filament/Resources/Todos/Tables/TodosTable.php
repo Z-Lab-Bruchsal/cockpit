b6c9f22b75c1b45587bad6d516ceeed08d99ee59
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-06-03 20:02:55 +0000

    Label und Filter für Todo-Tabelle

--- app/Filament/Resources/Todos/Tables/TodosTable.php
+++ app/Filament/Resources/Todos/Tables/TodosTable.php
@@ -2,12 +2,16 @@
 
 namespace App\Filament\Resources\Todos\Tables;
 
+use App\Models\Group;
+use App\Models\User;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class TodosTable
 {
@@ -16,34 +20,57 @@ class TodosTable
         return $table
             ->columns([
                 TextColumn::make('created_at')
+                    ->label('Erstellungsdatum')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('updated_at')
+                    ->label('Bearbeitet')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('name')
+                    ->label('Name')
                     ->searchable(),
                 TextColumn::make('user.name')
+                    ->label('Benutzer')
                     ->searchable(),
                 TextColumn::make('todoable.name')
                     ->label('Zugewiesen')
                     ->searchable(),
                 TextColumn::make('due_date')
+                    ->label('Todo-Datum')
                     ->date()
                     ->sortable(),
                 TextColumn::make('done_date')
+                    ->label('Erledigt am')
                     ->date()
                     ->sortable(),
                 TextColumn::make('follow_up')
+                    ->label('Wiedervorlage')
                     ->date()
                     ->sortable(),
                 IconColumn::make('review')
+                    ->label('Review')
                     ->boolean(),
             ])
             ->filters([
-                //
+                Filter::make('mine')->label('von mir oder für mich')
+                    ->default()
+                    ->query(function (Builder $query) {
+                        $query->where('user_id', filament()->auth()->user()->id)
+                            ->orWhere('todoable_type', User::class)->where('todoable_id', filament()->auth()->user()->id)
+                            ->orwhere('todoable_type', Group::class)->whereIn('todoable_id', User::find(filament()->auth()->user()->id)->groups()->get()->pluck('id'));
+                        return $query;
+                    }),
+                Filter::make('alleoffenen')
+                    ->default()
+                    ->label('nicht erledigt')
+                    ->query(function (Builder $query) {
+                        $query->where('done_date', null);
+                        return $query;
+                    }),
+
             ])
             ->recordActions([
                 EditAction::make(),
