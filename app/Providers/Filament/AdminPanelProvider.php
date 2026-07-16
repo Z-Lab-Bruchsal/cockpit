97f79c249ace140d1b5b2e907a058a31b40248b7
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-07-16 22:32:00 +0000

    remove account widged

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,7 +51,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
                 TimeClockWidget::class,
                 MyTodosWidget::class,
                 MyOrdersWidget::class,
