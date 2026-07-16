e88989d3df37874004376a157ad10ac8057e7191
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-07-16 22:54:11 +0000

    default date and time format in german

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
@@ -30,10 +32,28 @@ class AppServiceProvider extends ServiceProvider
                 ->persistSearchInSession()
                 ->persistSortInSession()
                 ->selectable()
-                ->defaultPaginationPageOption(50);
+                ->defaultPaginationPageOption(50)
+                ->defaultDateDisplayFormat('d.m.Y')
+                ->defaultDateTimeDisplayFormat('d.m.Y H:i')
+                ->defaultTimeDisplayFormat('H:i');
         });
         Column::configureUsing(function (Column $column): void {
             $column->toggleable();
+        });
+
+        // German date format for every DatePicker/DateTimePicker in the app.
+        // DatePicker extends DateTimePicker, so both registrations are needed:
+        // the DateTimePicker one applies first, then the DatePicker one overrides
+        // the format for date-only fields.
+        DateTimePicker::configureUsing(function (DateTimePicker $component): void {
+            $component
+                ->native(false)
+                ->displayFormat('d.m.Y H:i');
+        });
+        DatePicker::configureUsing(function (DatePicker $component): void {
+            $component
+                ->native(false)
+                ->displayFormat('d.m.Y');
         });
     }
 }
