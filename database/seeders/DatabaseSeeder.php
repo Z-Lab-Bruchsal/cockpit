6fec1faee63aa9d3ee8e6b6ba12476898be3a7d6
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2026-09-13 16:17:53 +0000

    include shieldseeder to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\ShieldSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -19,5 +20,9 @@ class DatabaseSeeder extends Seeder
         DB::table('orderstatuses')->insert(['name' => 'bestellt']);
         DB::table('orderstatuses')->insert(['name' => 'angekommen']);
         DB::table('orderstatuses')->insert(['name' => 'genommen']);
-    }
+
+        $this->call([
+            ShieldSeeder::class,
+        ]);
+ }
 }
